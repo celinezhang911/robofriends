4870f84cf8ca1ce156811cf507da7b2e4b53c76c
celinezhang911 <celinezhang21@gmail.com>
2019-11-17 14:42:09 -0800

    Updates

---
+++ precache-manifest.8b411c474f4116406c19f4ef87526915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11dbbd4991c73da442710be8b460c8e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "87a18e9d2ade6734b76a",
+    "url": "/robofriends/static/css/main.747c87b3.chunk.css"
+  },
+  {
+    "revision": "04130c833c2154f9d603",
+    "url": "/robofriends/static/js/2.ada142a8.chunk.js"
+  },
+  {
+    "revision": "87a18e9d2ade6734b76a",
+    "url": "/robofriends/static/js/main.bc0066e4.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  }
+]);
